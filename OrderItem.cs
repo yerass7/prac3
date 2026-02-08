d1013f0291ee4eee445aa32bc7aabb4ae45d87e9
Makhanbay Yerassyl <makhanbayerasyl@gmail.com>
2026-02-08 20:33:18 +0500

    Create OrderItem.cs

---
+++ OrderItem.cs
@@ -0,0 +1,16 @@
+public class OrderItem
+{
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+
+    public OrderItem(Product product, int quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+
+    public double GetTotal()
+    {
+        return Product.Price * Quantity;
+    }
+}
